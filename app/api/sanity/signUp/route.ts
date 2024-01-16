e4072a0f4c86d1f57878845127397214d3ff0937
Howard <yeahaur@gmail.com>
2024-01-16 01:50:33 -0500

    trivial modify

--- app/api/sanity/signUp/route.ts
+++ app/api/sanity/signUp/route.ts
@@ -1,4 +1,5 @@
 import { signUpHandler } from "next-auth-sanity";
 import sanityClient from "@/src/libs/sanity";
+import { SanityClient } from "sanity";
 
-export const POST = signUpHandler(sanityClient);
+export const POST = signUpHandler(sanityClient: SanityClient);
