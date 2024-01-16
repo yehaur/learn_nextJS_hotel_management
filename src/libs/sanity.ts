aeaacbe696bfa0b8ebfe91a2fb48bf086a2edf24
Howard <yeahaur@gmail.com>
2024-01-16 01:45:34 -0500

    trivial modify

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -5,7 +5,7 @@ const sanityClient: SanityClient = createClient({
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
     useCdn: process.env.NODE_ENV === 'production',
     token: process.env.SANITY_STUDIO_TOKEN as string,
-    apiVersion: '2024-01-04',
+    apiVersion: '2024-01-16',
 });
 
 export default sanityClient;
