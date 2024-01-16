395ab25d76cf4049eb7ae001089e6b41965050e3
Howard <yeahaur@gmail.com>
2024-01-16 01:37:07 -0500

    add type: SanityClient type in src/libs/sanity.tx

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -1,6 +1,6 @@
-import { createClient } from "next-sanity";
+import { SanityClient, createClient } from "next-sanity";
 
-const sanityClient = createClient({
+const sanityClient: SanityClient = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
     useCdn: process.env.NODE_ENV === 'production',
